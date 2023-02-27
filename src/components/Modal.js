dec160b777be466879c70a929e147dc5f5139529
yishak <yishakabrham621@gmail.com>
2023-02-27 22:56:57 +0300

    commit

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,33 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
+
 //declaration
 const modalDOM = document.getElementById('modal');
-//on click callback fun. but we dont want the event propagate from its child so we should use stopPropagation
-const closeModal = () => {
-  return history.push('/');
-};
+
 //event propagtation[bubbling -from e.target to final parent] stopper
 const stopPropagation = (e) => {
   return e.stopPropagation();
 };
-const Modal = ({ name }) => {
+const Modal = ({ title, content, actions, onDismiss }) => {
   //createPortal take two argument -1st block is the JSX we want to render screen
   //2nd one is a place where we want to portal
   return ReactDOM.createPortal(
-    <div onClick={closeModal} className="ui dimmer modals visible active">
+    <div onClick={onDismiss} className="ui dimmer modals visible active">
       <div
         onClick={stopPropagation}
         className="ui standard modal visible active"
       >
-        <div className="header">{name} Stream</div>
-        <div className="content">
-          Are you sure you want to {name} this stream?
-        </div>
-        <div className="actions">
-          <button className="ui primary button">{name}</button>
-          <button className="ui button">Cancel</button>
-        </div>
+        <div className="header">{title} </div>
+        <div className="content">{content}</div>
+        <div className="actions">{actions}</div>
       </div>
     </div>,
     modalDOM
--- src/components/streams/Streamdelete.js
+++ src/components/streams/Streamdelete.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
+
+//on click callback fun. but we dont want the event propagate from its child so we should use stopPropagation
+const closeModal = () => {
+  return history.push('/');
+};
 
 const StreamDelete = () => {
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
   return (
     <div>
       Stream delete
-      <Modal name="Delete" />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream"
+        actions={actions}
+        onDismiss={closeModal}
+      />
     </div>
   );
 };
 export default StreamDelete;
+//React fragment is a JSX looking element that allow us to return a multiple elments inside[instead of Div]
+//it is not rendered in the elements so if we inspect we cant find it
